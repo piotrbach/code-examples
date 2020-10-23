fac5391939cbfb52402424893e6e7b44d792614e
Piotr Bach <piotr.bach.it@gmail.com>
2020-10-23 13:57:40 +0200

    Umbraco sql - list users groups

---
+++ umbraco/umbraco-list-users-groups.sql
@@ -0,0 +1,9 @@
+﻿SELECT umbracoUser.id,
+       umbracoUser.userLogin,
+       umbracoUser.userName,
+       umbracoUserGroup.userGroupName,
+       umbracoUserGroup.userGroupAlias
+FROM [umbracoUser] umbracoUser
+JOIN [umbracoUser2UserGroup] umbracoUser2UserGroup ON umbracoUser.id = umbracoUser2UserGroup.userId
+JOIN [umbracoUserGroup] umbracoUserGroup ON umbracoUserGroup.id = umbracoUser2UserGroup.userGroupId
+ORDER BY umbracoUser.userName ASC
